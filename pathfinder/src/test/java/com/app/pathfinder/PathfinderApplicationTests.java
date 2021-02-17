e41521dd8e05b94121b9abeb56ebfb41997f358b
Jyoungho <youngh0910@gmail.com>
2021-02-17 19:53:54 +0900

    newproject

---
+++ pathfinder/src/test/java/com/app/pathfinder/PathfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.pathfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
