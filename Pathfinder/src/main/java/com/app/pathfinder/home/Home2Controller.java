c270ab7261a680c3ff63ec8449c465fe49f53018
dongj <dongj@DESKTOP-79HK7HF>
2021-02-24 14:04:47 +0900

    home2

---
+++ Pathfinder/src/main/java/com/app/pathfinder/home/Home2Controller.java
@@ -0,0 +1,14 @@
+package com.app.pathfinder.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Home2Controller {
+	
+	@RequestMapping("/home2")
+	public String home2() {
+		return "/WEB-INF/home/home2.jsp";
+	}
+	
+}
