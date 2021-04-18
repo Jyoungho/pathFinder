c42d7570ebe96919c9752aa0070cbc2b9fba6e1a
Jyoungho <youngh0910@gmail.com>
2021-04-18 21:42:55 +0900

    main user아이디 표시할 수 있게 parameter에 Principal 추가

--- Pathfinder/src/main/java/com/app/pathfinder/controller/main/Maincontroller.java
+++ Pathfinder/src/main/java/com/app/pathfinder/controller/main/Maincontroller.java
@@ -1,13 +1,20 @@
 package com.app.pathfinder.controller.main;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Maincontroller {
 	
 	@RequestMapping("/main")
-	public String main() {
+	public String main(Model model, Principal principal) {
+		if(principal == null) return "main";
+		String username = principal.getName();
+		model.addAttribute("username", username);
+
 		return "main";
 	}
 }
